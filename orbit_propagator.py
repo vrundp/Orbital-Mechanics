4ff79a16abe2fba1fb532d28b010bddbea787c48
VRUND <34451140+vrundp@users.noreply.github.com>
2021-12-19 11:33:42 -0600

    Add files via upload

---
+++ orbit_propagator.py
@@ -0,0 +1,78 @@
+# Copyright (c) 2022 Vrund Patel, USA
+#
+import numpy as np
+import matplotlib.pyplot as plt
+import planetary_data as pd
+
+from scipy.integrate import ode
+
+class OrbitPropagator:
+
+	def __init__(self, r0, v0, t0, tf, dt, cb = pd.earth, J2 = False, CR3BP = False):
+
+		self.r0 = r0
+		self.v0 = v0
+		self.t0 = t0
+		self.tf = tf
+		self.dt = dt
+		self.tspan = self.tf - self.t0
+		self.cb = cb
+		self.J2 = J2
+		self.CR3BP = CR3BP
+
+	def propagate(self):
+		
+		self.times = np.linspace(self.t0, self.tf, int((self.tspan / self.dt) + 1))
+
+		self.Y0 = np.zeros(6)
+		self.Y0[:3] = self.r0
+		self.Y0[3:] = self.v0
+
+		self.rv = ode(self.derivFxn)
+		self.rv.set_integrator('dopri5', rtol = 1e-10, atol = 1e-20)
+		self.rv.set_initial_value(self.Y0, self.t0)
+
+		self.Y = np.zeros((len(self.times), len(self.Y0)))
+		self.Y[0,:] = self.Y0
+
+		for i in range(1, len(self.times)):
+
+			self.Y[i, :] = self.rv.integrate(self.times[i]) 
+
+			if not self.rv.successful():
+
+				raise RuntimeError("Could not integrate.")
+
+		return self.Y
+
+	def derivFxn(self, t, Y):
+    
+		Y = np.array(Y)
+		dY = np.zeros(Y.shape)
+		    
+		rx, ry, rz, vx, vy, vz = Y
+		r_vec = np.array([rx, ry, rz])
+		r = np.linalg.norm(r_vec)
+
+		if self.J2 == False:
+
+			ax, ay, az = (-self.cb["mu"] / r**3) * r_vec
+
+		else:
+
+			ax = (-self.cb["mu"] * rx / r**3) * (1 - self.cb["J2"] * 1.5 * (self.cb["radius"]**2 / r**2) * (5 * (rz**2 / r**2) - 1))
+			ay = (-self.cb["mu"] * ry / r**3) * (1 - self.cb["J2"] * 1.5 * (self.cb["radius"]**2 / r**2) * (5 * (rz**2 / r**2) - 1))
+			az = (-self.cb["mu"] * rz / r**3) * (1 - self.cb["J2"] * 1.5 * (self.cb["radius"]**2 / r**2) * (5 * (rz**2 / r**2) - 3))
+
+		dY = np.array([vx,vy,vz,ax,ay,az])
+
+		return dY
+
+
+r_0 = np.array([6667.998, -831.435, -1531.486])
+v_0 = np.array([-1.210185, -7.384715, -1.138699])
+
+orbit1 = OrbitPropagator(r_0,v_0, 0, 86400, 100, J2 = True)
+print(orbit1.propagate())
+
+
---
+++ planetary_data.py
@@ -0,0 +1,145 @@
+# Copyright (c) 2022 Vrund Patel, USA
+#
+# This file contains constsants and enumerations.
+# Data is extracted from Appendix A of "Orbital Mechanics for Engineering Students".
+
+sun = {
+	
+	"name": "Sun",
+	"mu": 132.712e9,
+	"mass": 1.989e30,
+	"radius": 696000
+
+}
+
+mercury = {
+	
+	"name": "Mercury",
+	"mu": 22030,
+	"mass": 330.2e21,
+	"radius": 2440,
+	"sma": 57.91e6,
+	"ecc": 0.2056,
+	"period": 87.97*24*3600,
+	"soi": 112000
+
+}
+
+venus = {
+	
+	"name": "Venus",
+	"mu": 324900,
+	"mass": 4.869e24,
+	"radius": 6052,
+	"sma": 108.2e6,
+	"ecc": 0.0067,
+	"period": 224.7*24*3600,
+	"soi": 616000
+
+}
+
+earth = {
+	
+	"name": "Earth",
+	"mu": 398600.4415,
+	"mass": 5.974e24,
+	"radius": 6378.1363,
+	"sma": 149.6e6,
+	"ecc": 0.0167,
+	"period": 365.256*24*3600,
+	"soi": 925000,
+	"J2": 0.00108248
+
+}
+
+moon = {
+	
+	"name": "Moon",
+	"mu": 4903,
+	"mass": 73.48e21,
+	"radius": 1737,
+	"sma": 384.4e3,
+	"ecc": 0.0549,
+	"period": 27.322*24*3600,
+	"soi": 66200
+
+}
+
+mars = {
+	
+	"name": "Mars",
+	"mu": 42828,
+	"mass": 641.9e21,
+	"radius": 3396,
+	"sma": 227.9e6,
+	"ecc": 0.0935,
+	"period": 1.881*365.256*24*3600,
+	"soi": 577000
+
+}
+
+jupiter = {
+	
+	"name": "Jupiter",
+	"mu": 126.686e6,
+	"mass": 1.899e27,
+	"radius": 71490,
+	"sma": 778.6e6,
+	"ecc": 0.0489,
+	"period": 11.86*365.256*24*3600,
+	"soi": 48.2e6
+
+}
+
+saturn = {
+	
+	"name": "Saturn",
+	"mu": 37.931e6,
+	"mass": 568.5e24,
+	"radius": 60270,
+	"sma": 1.433e9,
+	"ecc": 0.0565,
+	"period": 29.46*365.256*24*3600,
+	"soi": 54.8e6
+
+}
+
+uranus = {
+	
+	"name": "Uranus",
+	"mu": 5.794e6,
+	"mass": 86.83e24,
+	"radius": 25560,
+	"sma": 2.872e9,
+	"ecc": 0.0457,
+	"period": 84.01*365.256*24*3600,
+	"soi": 51.8e6
+
+}
+
+neptune = {
+	
+	"name": "Neptune",
+	"mu": 6.8351e6,
+	"mass": 102.4e24,
+	"radius": 24760,
+	"sma": 4.495e9,
+	"ecc": 0.0113,
+	"period": 164.8*365.256*24*3600,
+	"soi": 86.6e6
+
+}
+
+pluto = {
+	
+	"name": "Pluto",
+	"mu": 830,
+	"mass": 12.5e21,
+	"radius": 1195,
+	"sma": 5.870e9,
+	"ecc": 0.2444,
+	"period": 247.7*365.256*24*3600,
+	"soi": 3.08e6
+
+}
+
